6d14a4a70edff8af2f496f3076307c2c7c3f1538
Mikulas Strelecky <mikulas.strelecky@gmail.com>
2018-12-25 13:44:31 +0100

    Typo 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
-            HELLO FROM IWAY CLOUD PLATFORM !!! 
+            HELLO FROM IWAY CLOUD!!! 
           </p>
           <a
             className="App-link"
